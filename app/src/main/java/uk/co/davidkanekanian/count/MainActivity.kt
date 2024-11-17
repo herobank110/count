b76e6054cdff6fba30f91ccc0de41a6abed927b4
David Kanekanian <dkanekanian1@gmail.com>
2024-11-17 14:14:37 +0000

    sounds

--- app/src/main/java/uk/co/davidkanekanian/count/MainActivity.kt
+++ app/src/main/java/uk/co/davidkanekanian/count/MainActivity.kt
@@ -11,9 +11,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -36,7 +37,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     CountTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(modifier = Modifier.fillMaxSize()) {
+                innerPadding ->
             AnimalList(Modifier.padding(innerPadding).fillMaxSize())
         }
     }
@@ -45,21 +47,50 @@ fun App() {
 @Composable
 fun AnimalList(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Farm Animals")
-        TextButton(onClick = { playSound(context) }, shape = CircleShape) {
-            Image(
-                    painter = painterResource(id = R.drawable.cow),
-                    contentDescription = "Cow",
-                    modifier = Modifier.size(150.dp).padding(16.dp)
+
+    val animals =
+            listOf(
+                    mapOf("name" to "camel", "sound" to R.raw.camel, "image" to R.drawable.camel),
+                    mapOf("name" to "cat", "sound" to R.raw.cat, "image" to R.drawable.cat),
+                    mapOf("name" to "cow", "sound" to R.raw.cow, "image" to R.drawable.cow),
+                    mapOf(
+                            "name" to "dolphin",
+                            "sound" to R.raw.dolphin,
+                            "image" to R.drawable.dolphin
+                    ),
+                    mapOf("name" to "duck", "sound" to R.raw.duck, "image" to R.drawable.duck),
+                    mapOf(
+                            "name" to "peacock",
+                            "sound" to R.raw.peacock,
+                            "image" to R.drawable.peacock
+                    ),
+                    mapOf(
+                            "name" to "penguin",
+                            "sound" to R.raw.penguin,
+                            "image" to R.drawable.penguin
+                    ),
+                    mapOf("name" to "pig", "sound" to R.raw.pig, "image" to R.drawable.pig),
+                    mapOf("name" to "sheep", "sound" to R.raw.sheep, "image" to R.drawable.sheep),
             )
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        animals.forEach { animal ->
+            TextButton(
+                    onClick = { playSound(context, animal["sound"] as Int) },
+                    shape = CircleShape
+            ) {
+                Image(
+                        painter = painterResource(id = animal["image"] as Int),
+                        contentDescription = animal["name"] as String,
+                        modifier = Modifier.size(150.dp).padding(16.dp)
+                )
+            }
         }
     }
 }
 
-fun playSound(context: Context?) {
+fun playSound(context: Context?, sound: Int) {
     // simple version:
-    var mediaPlayer = MediaPlayer.create(context, R.raw.cow)
+    var mediaPlayer = MediaPlayer.create(context, sound)
     // complex version:
     // var mediaPlayer =
     //         MediaPlayer().apply {
